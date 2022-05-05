789847188515f4e02ceee2a1f6e3e5cb8c01342c
Ella Mae Gading <92125395+gadinge1@users.noreply.github.com>
2022-05-05 18:36:19 -0400

    Update 20220426192724-create-user-table.js

--- migrations/20220426192724-create-user-table.js
+++ migrations/20220426192724-create-user-table.js
@@ -3,11 +3,12 @@
 const { all } = require("../controllers/notes_controller");
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) { 
     // code that will execute when running the migration
     await queryInterface.createTable('UserTables', {
+      // Query interface commands can be used within the up and down methods of migration.
       userId: {
-        allowNull: false,
+        allowNull: false, // Disallowing null values
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
