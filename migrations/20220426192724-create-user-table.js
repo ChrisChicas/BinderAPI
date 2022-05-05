bd3808558ed70af9f0872d8bd0e0a2e7d0567da5
Ella Mae Gading <92125395+gadinge1@users.noreply.github.com>
2022-05-05 19:50:06 -0400

    Update 20220426192724-create-user-table.js

--- migrations/20220426192724-create-user-table.js
+++ migrations/20220426192724-create-user-table.js
@@ -5,23 +5,23 @@ const { all } = require("../controllers/notes_controller");
 module.exports = {
   async up(queryInterface, Sequelize) { 
     // code that will execute when running the migration. The up: function runs when applying the migration.
-    await queryInterface.createTable('UserTables', {
+    await queryInterface.createTable('UserTables', { // Table name 
       // Query interface commands can be used within the up and down methods of migration.
-      userId: {
+      userId: { // column name
         allowNull: false, // Disallowing null values
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userName: {
+      userName: { // column name
         type: Sequelize.STRING,
         allowNull: false
       },
-      password: {
+      password: { // column name
         type: Sequelize.STRING,
         allowNull: false
       },
-      displayName: {
+      displayName: { // column name
         type: Sequelize.STRING,
         allowNull: false
       }
@@ -32,3 +32,6 @@ module.exports = {
     await queryInterface.dropTable('UserTables');
   }
 };
+// Migrations can be generated using the sequelize migration command. 
+// Migrations is a way to update tables without having to drop the entire database. 
+// We can write custom migrations to update tables by utilizing the Sequelize query interface.
